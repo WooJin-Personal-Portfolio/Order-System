2a9cb4c8824a5d06e9980c5a1ac370667c469e67
jwj <dnwls16071@naver.com>
2025-03-17 21:54:11 +0900

    [feat] 상품 조회 - JPA 비관적 락 추가

--- src/main/java/com/jwj/order/repository/ItemRepository.java
+++ src/main/java/com/jwj/order/repository/ItemRepository.java
@@ -1,7 +1,18 @@
 package com.jwj.order.repository;
 
 import com.jwj.order.domain.Item;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+	// 기존 메서드는 유지하고, 비관적 락을 적용한 메서드 추가
+	@Lock(LockModeType.PESSIMISTIC_WRITE)
+	@Query("select i from Item i where i.id = :id")
+	Optional<Item> findByIdWithPessimisticLock(@Param("id") Long id);
 }
